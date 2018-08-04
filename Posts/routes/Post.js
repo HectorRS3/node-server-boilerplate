d83ad544df4ffc8b506ab6661c0dc60131e2f72c
Juan De Jesus <jdejesus93@gmail.com>
2018-08-04 11:30:12 -0400

    Token verification now works.

--- Posts/routes/Post.js
+++ Posts/routes/Post.js
@@ -1,8 +1,52 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/Post');
+const jwt = require("jsonwebtoken");
+const user = require("../../Accounts/models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", function(req, res){ postController.getAllPosts(req, res)})
-.post("/post", function(req, res){ postController.createNewPost(req, res) });
+router.get("/", function (req, res) {
+  const { token } = req.headers;
+  jwt.verify(token, process.env.SECRET, function (err, decoded) {
+    if (err) {
+      res.status(401).send({ message: "You must provide a valid authentication token." });
+    } else {
+      user.findOne({ username: decoded.username }, 'password')
+        .then(person => {
+          bcrypt.compare(decoded.password, person.password, function (err, pass) {
+            if (err) return err;
+            if (pass === null) {
+              res.status(404).send({message: "Incorrect username or password."});
+            } else {
+              postController.getAllPosts(req, res);
+            }
+          });
+        })
+        .catch(err => console.log(`ERROR: ${err}`));
+    }
+  })
+})
+
+.post("/post", function (req, res) { 
+  const {token} = req.headers;
+  jwt.verify(token, process.env.SECRET, function(err, decoded){
+    if(err) {
+      res.status(401).send({message: "You must provide a valid authentication token."});
+    } else {
+      user.findOne({username: decoded.username}, 'password')
+      .then(person => {
+        bcrypt.compare(decoded.password, person.password, function(err, pass){
+          if(err) return err;
+          if(pass === null) {
+            res.status(401).send({message: "Incorrect username or password."})
+          } else {
+            postController.createNewPost(req, res);
+          }
+        });
+      })
+      .catch(err => console.log(`ERROR: ${err}`));
+    }
+  })
+ });
 
 module.exports = router;
